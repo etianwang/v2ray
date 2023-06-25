250162822d3962de8c6d2957b5c9dee261cf4f03
etianwang <etianwang@yahoo.com>
2023-06-25 21:48:45 +0800

    edited

--- go.sh
+++ go.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "1
 10
-8080
+12580
 
 
 
-" | bash <(curl -s -L https://raw.githubusercontent.com/xyz690/v2ray/master/install.sh)
+" | bash <(curl -s -L https://raw.githubusercontent.com/etianwang/v2ray/main/install.sh)
 v2ray url
